6a032c80464ed9a17314a201bba3e35622e177f3
Staroul <66063409+ACGNworld@users.noreply.github.com>
2022-12-13 11:54:42 +0000

    new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	modified:   untitled3/CMakeLists.txt 	new file:   untitled3/main 	modified:   untitled3/main.cpp

--- untitled3/main.cpp
+++ untitled3/main.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+static void *new_thread_start(void *arg)
+{
+ printf("新线程: 进程 ID<%d> 线程 ID<%lu>\n", getpid(), pthread_self());
+ return (void *)0;
+}
+int main(void)
+{
+ pthread_t tid;
+ int ret;
+ ret = pthread_create(&tid, NULL, new_thread_start, NULL);
+ if (ret) {
+ fprintf(stderr, "Error: %s\n", strerror(ret));
+ exit(-1);
+ }
+ printf("主线程: 进程 ID<%d> 线程 ID<%lu>\n", getpid(), pthread_self());
+ sleep(1);
+ exit(0);
 }
